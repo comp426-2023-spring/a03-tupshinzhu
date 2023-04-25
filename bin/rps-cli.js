9fe08d1f6b278c7885f1ad04b6f7b18419ac6c99
Tupshin Zhu <tup@ad.unc.edu>
2023-04-24 22:32:35 -0400

    rock

--- bin/rps-cli.js
+++ bin/rps-cli.js
@@ -1,1 +1,34 @@
 #!/usr/bin/env node
+
+import { playRPS } from "../lib/rps_game.js";
+import minimist from "minimist";
+
+const args = minimist(process.argv);
+if (args.h || args.help) {
+  console.log(`Usage: play-rps [SHOT]
+Play Rock Paper Scissors (RPS)
+  -h, --help      display this help message and exit
+  -r, --rules     display the rules and exit
+Examples:
+  play-rps        Return JSON with single player RPS result.
+                  e.g. {"player":"rock"}
+  play-rps rock   Return JSON with results for RPS played against a simulated opponent.
+                  e.g {"player":"rock","opponent":"scissors","result":"win"}`);
+  process.exit();
+}
+
+if (args.r || args.rules) {
+  console.log(`Rules for Rock Paper Scissors:
+  - Scissors CUTS Paper
+  - Paper COVERS Rock
+  - Rock CRUSHES Scissors`);
+  process.exit();
+}
+
+if (args._.length <= 2) {
+  console.log(JSON.stringify(playRPS()));
+} else if (args._.length > 3) {
+  console.error("ERROR: arguments out of range");
+} else {
+  console.log(JSON.stringify(playRPS(...args._.slice(2))));
+}
--- bin/rpsls-cli.js
+++ bin/rpsls-cli.js
@@ -1,1 +1,42 @@
 #!/usr/bin/env node
+
+import { rpsls } from "../lib/rpsls.js";
+import minimist from "minimist";
+
+const args = minimist(process.argv);
+
+if (args.h || args.help) {
+  console.log(`Usage: node-rpsls [SHOT]
+Play the Lizard-Spock Expansion of Rock Paper Scissors (RPSLS)!
+  -h, --help        display this help message and exit
+  -r, --rules       display the rules and exit
+Examples:
+  node-rpsls        Return JSON with single player RPSLS result.
+                    e.g. {"player":"rock"}
+  node-rpsls rock   Return JSON with results for RPSLS played against a simulated opponent.
+                    e.g {"player":"rock","opponent":"Spock","result":"lose"}`);
+  process.exit();
+}
+
+if (args.r || args.rules) {
+  console.log(`Rules for the Lizard-Spock Espansion of Rock Paper Scissors:
+  - Scissors CUTS Paper
+  - Paper COVERS Rock
+  - Rock SMOOSHES Lizard
+  - Lizard POISONS Spock
+  - Spock SMASHES Scissors
+  - Scissors DECAPITATES Lizard
+  - Lizard EATS Paper
+  - Paper DISPROVES Spock
+  - Spock VAPORIZES Rock
+  - Rock CRUSHES Scissors`);
+  process.exit();
+}
+
+if (args._.length <= 2) {
+  console.log(JSON.stringify(rpsls()));
+} else if (args._.length > 3) {
+  console.error("ERROR: arguments out of range");
+} else {
+  console.log(JSON.stringify(rpsls(...args._.slice(2))));
+}
