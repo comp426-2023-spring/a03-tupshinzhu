8f459e696c73104050b03fecd79703b36f433ae4
Tupshin Zhu <tup@ad.unc.edu>
2023-05-09 00:36:03 -0400

    fix rps rules message

--- bin/rps-cli.js
+++ bin/rps-cli.js
@@ -5,10 +5,13 @@ import minimist from "minimist"
 
 const args = minimist(process.argv)
 if (args.h || args.help) {
-    console.log(`Usage: node-rps [SHOT]
+    console.log(
+`Usage: node-rps [SHOT]
 Play Rock Paper Scissors (RPS)
+
   -h, --help      display this help message and exit
   -r, --rules     display the rules and exit
+
 Examples:
   node-rps        Return JSON with single player RPS result.
                   e.g. {"player":"rock"}
@@ -19,6 +22,7 @@ Examples:
 
 if (args.r || args.rules) {
     console.log(`Rules for Rock Paper Scissors:
+    
   - Scissors CUTS Paper
   - Paper COVERS Rock
   - Rock CRUSHES Scissors`)
