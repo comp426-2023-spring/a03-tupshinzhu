1af1f38280041f056e364e727994af4a841be342
Tupshin Zhu <tup@ad.unc.edu>
2023-05-09 01:15:21 -0400

    error message rps

--- bin/rps-cli.js
+++ bin/rps-cli.js
@@ -35,6 +35,25 @@ else{
     }
     else {
         console.error(`${args._[0]} is out of range.`);
-}
+        console.log(
+    `Usage: node-rps [SHOT]
+Play Rock Paper Scissors (RPS)
+            
+    -h, --help      display this help message and exit
+    -r, --rules     display the rules and exit
+            
+Examples:
+node-rps        Return JSON with single player RPS result.
+                e.g. {"player":"rock"}
+node-rps rock   Return JSON with results for RPS played against a simulated opponent.
+                e.g {"player":"rock","opponent":"scissors","result":"win"}`
+        );
+        console.log(`Rules for Rock Paper Scissors:
+    
+    - Scissors CUTS Paper
+    - Paper COVERS Rock
+    - Rock CRUSHES Scissors`
+        )
+    }   
 }
 
