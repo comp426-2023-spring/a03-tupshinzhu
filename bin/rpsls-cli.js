a78c49df8c1c0b6fdd12ce6e355a86d554ab221f
Tupshin Zhu <tup@ad.unc.edu>
2023-05-09 02:01:54 -0400

    rpsls error message fix

--- bin/rpsls-cli.js
+++ bin/rpsls-cli.js
@@ -1,18 +1,18 @@
 #!/usr/bin/env node
-import { rps } from "../lib/rpsls.js"
+import { rpsls } from "../lib/rpsls.js"
 import minimist from "minimist"
 
 const args = minimist(process.argv.slice(2))
 if (args.h || args.help) {
     console.log(
-`Usage: node-rps [SHOT]
+`Usage: node-rpsls [SHOT]
 Play the Lizard-Spock Expansion of Rock Paper Scissors (RPSLS)!
 
   -h, --help      display this help message and exit
   -r, --rules     display the rules and exit
 
 Examples:
-  node-rpsls        Return JSON with single player RPS result.
+  node-rpsls        Return JSON with single player RPSLS result.
                     e.g. {"player":"rock"}
   node-rpsls rock   Return JSON with results for RPSLS played against a simulated opponent.
                     e.g {"player":"rock","opponent":"Spock","result":"lose"}`)
@@ -34,7 +34,7 @@ else if (args.r || args.rules) {
     process.exit()
 }
 else{
-    let output = rps(args._[0])
+    let output = rpsls(args._[0])
 
     if (output != 'error') {
         console.log(JSON.stringify(output));
@@ -42,19 +42,21 @@ else{
     else {
         console.error(`${args._[0]} is out of range.`);
         console.log(
-    `Usage: node-rps [SHOT]
+    `Usage: node-rpsls [SHOT]
 Play the Lizard-Spock Expansion of Rock Paper Scissors (RPSLS)!
             
     -h, --help      display this help message and exit
     -r, --rules     display the rules and exit
             
 Examples:
-node-rpsls        Return JSON with single player RPS result.
+node-rpsls        Return JSON with single player RPSLS result.
                   e.g. {"player":"rock"}
 node-rpsls rock   Return JSON with results for RPSLS played against a simulated opponent.
                   e.g {"player":"rock","opponent":"Spock","result":"lose"}`
+                  
         );
-        console.log(`Rules for the Lizrd-Spock Espansion of Rock Paper Scissors:
+
+        console.log(`Rules for the Lizard-Spock Espansion of Rock Paper Scissors:
     
         - Scissors CUTS Paper
         - Paper COVERS Rock
